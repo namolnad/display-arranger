c9c456743d9db2b9cb38b18219edbcd2db72b43c
Dan L <daniel.h.loman@gmail.com>
2018-01-11 18:43:01 -0800

    Initial Commit

---
+++ DisplayArranger/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DisplayArranger
+//
+//  Created by Dan Loman on 1/11/18.
+//  Copyright © 2018 Daniel Loman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
